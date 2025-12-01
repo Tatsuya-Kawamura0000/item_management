395e199a42f84b1ba7683559e2331147a9e9f050
Tatsuya-Kawamura0000 <tatsuya.k89.0523kitaq17@gmail.com>
2025-12-01 23:27:42 +0900

    カレンダー修正　12/1

---
+++ src/main/resources/static/js/addForm.js
@@ -0,0 +1,7 @@
+$('#deadline, #purchaseDate').datepicker({
+    format: 'yyyy/mm/dd',
+    language: 'ja',
+    autoclose: true,
+    todayHighlight: true,
+    orientation: "bottom right"  // ★ここを追加！
+});
