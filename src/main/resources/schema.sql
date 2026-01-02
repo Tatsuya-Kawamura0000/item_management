7a9b9b5e34ab3fce678805182590ef34a627e372
Tatsuya-Kawamura0000 <tatsuya.k89.0523kitaq17@gmail.com>
2026-01-03 03:37:46 +0900

    schema.sql 編集

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS items;
 DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS shopping_list;
 
 CREATE TABLE categories (
     id serial PRIMARY KEY,
@@ -22,3 +23,18 @@ CREATE TABLE items (
         FOREIGN KEY (category_id)
         REFERENCES categories(id)
 );
+
+CREATE TABLE IF NOT EXISTS shopping_list (
+    id serial PRIMARY KEY,
+    item_id integer REFERENCES items(id),
+    user_id integer,  -- ユーザー管理している場合
+    status boolean DEFAULT true,
+    added_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    purchased_flg boolean DEFAULT false,
+    name varchar(50),
+    amount varchar(20),
+    category_id integer,
+    category_name varchar(50),
+    purchase_date date
+);
+
