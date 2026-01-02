cae4a32c3c40699c76bb5f86dff85e7abefd2723
Tatsuya-Kawamura0000 <tatsuya.k89.0523kitaq17@gmail.com>
2026-01-03 03:21:12 +0900

    data.sql 追加

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO categories (name) VALUES ('肉');
+INSERT INTO categories (name) VALUES ('野菜');
+INSERT INTO categories (name) VALUES ('魚');
+INSERT INTO categories (name) VALUES ('調味料');
+INSERT INTO categories (name) VALUES ('その他');
