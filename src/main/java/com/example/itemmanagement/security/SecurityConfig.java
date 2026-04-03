adfc0551347f1047b11a088b2f171adeb32d3f27
Tatsuya-Kawamura0000 <tatsuya.k89.0523kitaq17@gmail.com>
2026-04-03 11:52:31 +0900

    feat(user): ユーザー登録画面と遷移設定を実装

--- src/main/java/com/example/itemmanagement/security/SecurityConfig.java
+++ src/main/java/com/example/itemmanagement/security/SecurityConfig.java
@@ -10,21 +10,22 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .anyRequest().authenticated()   // 認証必須
-            )
-            .formLogin(form -> form
-            	    .loginPage("/login")
-            	    .defaultSuccessUrl("/users", true)
-            	    .permitAll()
-          
-            );
-        return http.build();
-    }
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	    http
+	        .csrf(csrf -> csrf.disable())
+	        .authorizeHttpRequests(auth -> auth
+	            .requestMatchers("/login", "/register").permitAll()  //  誰でもアクセス可能
+	            .anyRequest().authenticated()
+	        )
+	        .formLogin(form -> form
+	            .loginPage("/login")
+	            .defaultSuccessUrl("/users", true)
+	            .permitAll()
+	        );
+
+	    return http.build();
+	}
 
     @Bean
     public PasswordEncoder passwordEncoder() {
