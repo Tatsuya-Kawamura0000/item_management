83b8f966b6f9e22e20a99a180eea1d6bc3eecf2e
Tatsuya-Kawamura0000 <tatsuya.k89.0523kitaq17@gmail.com>
2026-04-06 15:22:41 +0900

    fix(user): 確認用パスワードフォーム追加とバリデーション修正

--- src/main/java/com/example/itemmanagement/controller/RegisterController.java
+++ src/main/java/com/example/itemmanagement/controller/RegisterController.java
@@ -1,7 +1,11 @@
 package com.example.itemmanagement.controller;
 
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,16 +20,31 @@ public class RegisterController {
 	private RegisterService registerService;
 	
 	@GetMapping("/register")
-    public String showRegisterForm() {
+    public String showRegisterForm(Model model) {
+		
+		model.addAttribute("registerForm", new RegisterForm());
+		
         return "register";
     }
 	
 	@PostMapping("/register")
-    public String registerUser(@ModelAttribute RegisterForm form) {
+	public String registerUser(@ModelAttribute @Valid RegisterForm form, BindingResult bindingResult, Model model) {
 
-		registerService.register(form);  // Serviceで登録処理
+		    // バリデーションエラーがある場合は再表示
+	        if (bindingResult.hasErrors()) {
+	            return "register";
+	        }
+		
+		
+	        // パスワード一致チェック
+	        if (!form.getPassword().equals(form.getConfirmPassword())) {
+	            model.addAttribute("confirmPassworderrorMessage", "パスワードが一致しません");
+	            return "register";
+	        }
 
-        return "redirect:/login";
-    }
+	        registerService.register(form);
+
+	        return "redirect:/login";
+	}
 
 }
