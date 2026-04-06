f9481d6b13dd8f4fc344abe66c10d0237d4165dc
Tatsuya-Kawamura0000 <tatsuya.k89.0523kitaq17@gmail.com>
2026-04-06 13:20:44 +0900

    fix(env): .envが無い環境でも起動できるようignoreIfMissingを追加

--- src/main/java/com/example/itemmanagement/ItemmanagementApplication.java
+++ src/main/java/com/example/itemmanagement/ItemmanagementApplication.java
@@ -11,12 +11,19 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class ItemmanagementApplication {
 
 	public static void main(String[] args) {
-		
-		Dotenv dotenv = Dotenv.load();
 
-	    System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
-	    System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
-		
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		if (dotenv.get("MAIL_USERNAME") != null) {
+			System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
+		}
+
+		if (dotenv.get("MAIL_PASSWORD") != null) {
+			System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
+		}
+
 		SpringApplication.run(ItemmanagementApplication.class, args);
 	}
 
